74f9029068e83ab537d4f43819d3229585b14dda
Rakeshpro9040 <57039842+Rakeshpro9040@users.noreply.github.com>
2021-08-10 11:23:03 +0530

    Rename Functions Folder

---
+++ Functions and Methods/first_class_function.py
@@ -0,0 +1,14 @@
+# Normal Function Assignment to a Vraible
+def square(x):
+    return x * x
+
+f = square(5)
+print(f) # 25
+
+# Now lets set a Function to a Variable
+def square(x):
+    return x * x
+
+f = square
+print(f(5)) # 25
+
